e2e8d165aaf9b979b6744a64607c7f314ed86dd1
lijun1999 <2562402511@qq.com>
2020-09-15 15:20:47 +0800

    修改简历内容

--- asset/js/index.js
+++ asset/js/index.js
@@ -79,9 +79,14 @@ $(function () {
 	styleBox.load("<div class='writecode introduce'>*/</div></p>");
 
 	//显示简历框
-	styleBox.addClass("#work-box", "box ");
+	
 
 	var workBox = new RESUME("work-box");
+	workBox.setClass("#work-box", [{
+		"name": "display",
+		"val": "block"
+	}]);
+	workBox.addClass("#work-box", "box ");
 
 	workBox.load("<h1 class='writecode'>李军</h1>");
 	workBox.load("<div class='writecode'>邮箱：2562402511@qq.com</div>");
@@ -105,10 +110,19 @@ $(function () {
 
 	//项目经历
 	workBox.load("<h2 class='writecode'>项目经历</h2>");
+
+	workBox.load("<ul>");
+	workBox.load("<div class='lr-box writecode'> <span class='lr-box-left project-title '>动态简历</span> <span class='project-time'>2019.11 - 2020.05</span> </div>");
+	workBox.load("<li class='writecode'>源码链接：<a href='https://github.com/lijun1999/resume' target='_blank'>https://github.com/lijun1999/resume</a></li>");
+	workBox.load("<li class='writecode'>预览链接：<a href='https://lijun1999.github.io/resume/' target='_blank'>https://lijun1999.github.io/resume/</a></li>");
+	workBox.load("<li class='writecode'>描述：该项目实现了一个简历的制作过程，利用CSS3实现过渡、动画、阴影和弹性布局。</li>");
+	workBox.load("</ul>");
+
 	workBox.load("<ul>");
 	workBox.load("<div class='lr-box writecode'> <span class='lr-box-left project-title '>流浪大师设备智能管理系统</span> <span class='project-time'>2019.11 - 2020.05</span> </div>");
+	workBox.load("<li class='writecode'>源码链接：<a href='https://github.com/lijun1999/lldsms' target='_blank'>https://github.com/lijun1999/lldsms</a></li>");
 	workBox.load("<li class='writecode'>流浪大师设备智能管理系统是一个设备管理网站，配合RFID硬件使用。</li>");
-	workBox.load("<li class='writecode'>主要负责系统网站的前端所有模块的开发，处理前端代码的系统性能问题和代码模块化问题，<br>提升了代码的可读性和健壮性.</li>");
+	workBox.load("<li class='writecode'>主要负责系统网站的前端所有模块的开发，处理前端代码的系统性能问题和代码模块化问题，提升了代码的可读性和健壮性.</li>");
 	workBox.load("<li class='writecode'>运用Vue中的指令和axios与后台接口对接，进行数据交互和页面渲染，实现功能模块的判断</li>");
 	workBox.load("<li class='writecode'>使用Element UI组件库减少项目工程量，加快开发进度.</li>");
 	workBox.load("<li class='writecode'>移动端使用uni-app生成H5页面和Android客户端</li>");
@@ -116,6 +130,7 @@ $(function () {
 
 	workBox.load("<ul>");
 	workBox.load("<div class='lr-box writecode'> <span class='lr-box-left project-title '>参观成信(未上线)</span> <span class='project-time'>2019.11 - 2020.05</span> </div>");
+	workBox.load("<li class='writecode'>源码链接：<a href='https://gitee.com/lijun1999/visitcx' target='_blank'>https://gitee.com/lijun1999/visitcx</a></li>");
 	workBox.load("<li class='writecode'>参观成信是成都信息工程大学的非官方导览小程序，可为公众提供校园导览等服务。</li>");
 	workBox.load("<li class='writecode'>主要负责实现首页，学校介绍页，校园导览页，校园美景的渲染等业务的要求。</li>");
 	workBox.load("<li class='writecode'>使用flex弹性布局解决界面相关的对齐居中等问题。</li>");
@@ -123,14 +138,6 @@ $(function () {
 	workBox.load("<li class='writecode'>使用腾讯地图API实现校园景点标记和路线规划</li>");
 	workBox.load("</ul>");
 
-	workBox.load("<ul>");
-	workBox.load("<div class='lr-box writecode'> <span class='lr-box-left project-title '>FE3O4</span> <span class='project-time'>2019.11 - 2020.05</span> </div>");
-	workBox.load("<li class='writecode'>FE3O4是一个基于websocket的即时通讯app</li>");
-	workBox.load("<li class='writecode'>主要负责实现首页，学校介绍页，校园导览页，校园美景的渲染等业务的要求。</li>");
-	workBox.load("<li class='writecode'>主要负责实现登录注册页、好友页、搜索页、用户详情页、聊天页面的渲染等业务的要求。</li>");
-	workBox.load("<li class='writecode'>登录注册使用正则表达式进行格式判断</li>");
-	workBox.load("<li class='writecode'>搜索好友时通过防抖来减少不必要的损耗</li>");
-	workBox.load("</ul>");
 
 
 	workBox.load("<h2 class='writecode'>个人总结</h2>");
@@ -179,8 +186,8 @@ $(function () {
 	styleBox.load("<div class='introduce writecode'>&nbsp;*最后非常感谢您愿意时间看完我的简历！</div>");
 	styleBox.load("<div class='introduce writecode'>&nbsp;*/</div></p>");
 	styleBox.setClass("#style-box", [{
-		"name": "flex",
-		"val": "0"
+		"name": "display",
+		"val": "none"
 	}]);
 
 });
@@ -192,11 +199,11 @@ class RESUME {
 	}
 	load(code, second) {
 		const id = this._id;
-		let _second = second ? second : 1000;
+		let _second = second ? second : 500;
 		_second = time + _second;
 		setTimeout(function () {
-			$('#' + id).scrollTop($('#' + id)[0].scrollHeight);
 			$("#" + id).append(code);
+			$('#' + id).scrollTop($('#' + id)[0].scrollHeight);
 		}, _second);
 		time = _second;
 	}
